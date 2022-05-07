434d069adeb7bc04924d498e64dee428fa0beb8e
Petar Partaloski <ppartaloski@yahoo.com>
2022-05-07 20:16:48 +0200

    Added localization for Macedonian users

--- src/main/java/com/example/expirydatetrackerapi/web/controller/HomeController.java
+++ src/main/java/com/example/expirydatetrackerapi/web/controller/HomeController.java
@@ -3,12 +3,18 @@ package com.example.expirydatetrackerapi.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
     @GetMapping("")
-    public String getHomePage(){
-        return "home-page";
+    public String getHomePage(@RequestParam(required = false) String lang){
+        if(lang == null || lang.contentEquals("EN")){
+            return "home-page-en";
+        }
+        else{
+            return "home-page-mk";
+        }
     }
 }
