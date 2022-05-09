4f98c5b15ed52d62db90a3e25be31f436194f032
Petar Partaloski <ppartaloski@yahoo.com>
2022-05-09 02:39:59 +0200

    Fixed manufacturers bug

--- src/main/java/com/example/expirydatetrackerapi/models/Manufacturer.java
+++ src/main/java/com/example/expirydatetrackerapi/models/Manufacturer.java
@@ -2,10 +2,7 @@ package com.example.expirydatetrackerapi.models;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "manufacturers")
@@ -13,6 +10,7 @@ import javax.persistence.Table;
 public class Manufacturer {
     @Id
     @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name="name")
